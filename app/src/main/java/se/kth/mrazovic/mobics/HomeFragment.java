4f17fc436b5fa7430cef0078405e58ed741e0141
Petar Mrazovic <petar.mrazovic@gmail.com>
2015-06-11 21:42:48 +0200

    Removed unneeded dependencies in HomeFragment.

--- app/src/main/java/se/kth/mrazovic/mobics/HomeFragment.java
+++ app/src/main/java/se/kth/mrazovic/mobics/HomeFragment.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
